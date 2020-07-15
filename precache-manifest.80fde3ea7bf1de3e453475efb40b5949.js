b40ae2a2bcd9c262db6d5f5d514084213f34085b
Nero Krase <nerokrasegames@gmail.com>
2020-07-15 10:49:25 +0300

    commit

---
+++ precache-manifest.80fde3ea7bf1de3e453475efb40b5949.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ad42fa62c4f63408fcd4a1e5693e09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcc1cf5f5b5845910198",
+    "url": "/static/css/main.a85eb70b.chunk.css"
+  },
+  {
+    "revision": "077b1c63ded6bbae6876",
+    "url": "/static/js/2.e38c0d4b.chunk.js"
+  },
+  {
+    "revision": "d3651c946796eb2d839d7d6b8a1dbaad",
+    "url": "/static/js/2.e38c0d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc1cf5f5b5845910198",
+    "url": "/static/js/main.34c01463.chunk.js"
+  },
+  {
+    "revision": "c26937116b1cbcde6c28",
+    "url": "/static/js/runtime-main.ec4a4ebe.js"
+  },
+  {
+    "revision": "39de8040c07a662d3ee5aea2f2473081",
+    "url": "/static/media/About.39de8040.jpg"
+  },
+  {
+    "revision": "c67cd55f09a43568829b8a9ff3a29a7a",
+    "url": "/static/media/Carousel-1.c67cd55f.jpg"
+  },
+  {
+    "revision": "b03924b74eda3795c6a0b11293e627d2",
+    "url": "/static/media/Carousel-2.b03924b7.jpg"
+  },
+  {
+    "revision": "1dd4cf510f728fea06558dfd298b8ff0",
+    "url": "/static/media/Carousel-3.1dd4cf51.jpg"
+  },
+  {
+    "revision": "818d9a487d37b5a9c2e87330d35ccb16",
+    "url": "/static/media/Carousel-4.818d9a48.jpg"
+  },
+  {
+    "revision": "40a055297f69a7afb8aa1358497778a7",
+    "url": "/static/media/Carousel-5.40a05529.jpg"
+  },
+  {
+    "revision": "7440e19961f94e87dedf549e3bb65bcd",
+    "url": "/static/media/facebook-logo.7440e199.svg"
+  },
+  {
+    "revision": "90ba0fcc2afef8622aee22c700e5139e",
+    "url": "/static/media/gallery1.90ba0fcc.jpg"
+  },
+  {
+    "revision": "da3e2a879b07288adaeaa4075c70a605",
+    "url": "/static/media/gallery2.da3e2a87.jpg"
+  },
+  {
+    "revision": "b3728762443ac63dd4cbb0d0410d9aa7",
+    "url": "/static/media/gallery3.b3728762.jpg"
+  },
+  {
+    "revision": "b340ac92bc805769abdb5cea7565dcdd",
+    "url": "/static/media/gallery4.b340ac92.jpg"
+  },
+  {
+    "revision": "bf02eb36d946cfe82f5d446d8999a88a",
+    "url": "/static/media/gallery5.bf02eb36.jpg"
+  },
+  {
+    "revision": "ef888d652e3c697df579d51986efb4c8",
+    "url": "/static/media/gallery6.ef888d65.jpg"
+  },
+  {
+    "revision": "b9bd82d977305fb243f20e42b8f96f50",
+    "url": "/static/media/gallery7.b9bd82d9.jpg"
+  },
+  {
+    "revision": "4e370f1348636bfae95c35709a420488",
+    "url": "/static/media/gallery8.4e370f13.jpg"
+  },
+  {
+    "revision": "752c2ff95473b9a5f73cb79c2e505b1f",
+    "url": "/static/media/gallery9.752c2ff9.jpg"
+  },
+  {
+    "revision": "515feeaaed3a71d8ba95078d18142b0a",
+    "url": "/static/media/inst-logo.515feeaa.svg"
+  },
+  {
+    "revision": "73e63c9a21c5da97fd241bbe71853a75",
+    "url": "/static/media/joystick.73e63c9a.svg"
+  },
+  {
+    "revision": "04e3d0a379c30fd61a3ec3b10d646595",
+    "url": "/static/media/patreon-logo.04e3d0a3.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "de3756b2938c030e41ad6b08a78797af",
+    "url": "/static/media/team-portrait1.de3756b2.jpg"
+  },
+  {
+    "revision": "31c29dbaf0b63ce0ab50eb1dc808eede",
+    "url": "/static/media/team-portrait2.31c29dba.jpg"
+  },
+  {
+    "revision": "998a3b02159751908d664245902ba9f1",
+    "url": "/static/media/team-portrait3.998a3b02.jpg"
+  },
+  {
+    "revision": "676e404377bcdcb5ec0f86b3c91146ee",
+    "url": "/static/media/team-portrait4.676e4043.jpg"
+  },
+  {
+    "revision": "62a588594a948000b6393e6421730a5a",
+    "url": "/static/media/twit-logo.62a58859.svg"
+  }
+]);
